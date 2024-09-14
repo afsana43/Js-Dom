3026232050416ec7ffa182a8899f2986a2199a60
afsana43 <afsanahamidmim70@gmail.com>
2024-09-14 23:37:41 +0600

    terversing getelementbyid and class

--- dom.js
+++ dom.js
@@ -1,10 +1,22 @@
 // console.log('hello from external js');
+
+// tagName selector
 const myNamesCollections = document.getElementsByTagName('li');
 // console.log(myNamesCollections);
 
 
 // loop for of using to find li text
 for(const myNamesCollection of myNamesCollections){
-    console.log(myNamesCollection.innerText);
+    // console.log(myNamesCollection.innerText);
     
 }
+
+// id selector
+const placeLists = document.getElementById('favourite-place');
+// console.log(placeLists.innerText);
+
+// class selector 
+const fvrtFood = document.getElementsByClassName('favouirte-fruits');
+console.log(fvrtFood);
+
+
